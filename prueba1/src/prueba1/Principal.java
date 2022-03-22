42c1728d74c2ffa309ce1e55a37eb17b51d78cab
Usuario2 <fatimarodriguez@iesmarquesdecomares.org>
2022-03-22 21:14:16 +0100

    Primera modificación en principal. Parte 2.

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -2,7 +2,7 @@ package prueba1;
 
 public class Principal {
 	public static void main(String[] args) {
-		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git modifico 1");
 
 	}
 
