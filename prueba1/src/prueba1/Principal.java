9e8d51ad84e508d8b9e9e88b0cba5a8c637983f6
Usuario2 <fatimarodriguez@iesmarquesdecomares.org>
2022-03-28 18:48:25 +0200

    Añado línea en la clase principal

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -3,6 +3,7 @@ package prueba1;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git modifico 2");
+		System.out.println("Pruebas Git. Se añade Cambio1");
 
 	}
 
